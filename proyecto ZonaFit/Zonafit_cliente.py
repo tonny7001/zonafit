488b3dced8ba3cf393df44a598f0e2ce86603f01
Jorge Mosquera <jorge_mosquera6@hotmail.com>
2026-06-11 22:41:37 -0500

    avance

--- proyecto ZonaFit/Zonafit_cliente.py
+++ proyecto ZonaFit/Zonafit_cliente.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from tkinter import ttk
+from tkinter import messagebox, ttk
 
 from cliente_dao import ClienteDAO
 
@@ -16,6 +16,7 @@ class Interfaz(tk.Tk):
         self.mostrar_titulo()
         self.mostrar_formulario()
         self.mostrar_tablas()
+        self.mostrar_botones()
 
     # ===================
     # MOSTRAR VENTANA
@@ -35,6 +36,7 @@ class Interfaz(tk.Tk):
             foreground="#F5F5F5",
             fieldbackground="#06202B",
         )
+        self.protocol("WM_DELETE_WINDOW", self.salir)
 
     # ===================
     # CONFIGURAR GRID
@@ -49,6 +51,7 @@ class Interfaz(tk.Tk):
     # MOSTRAR TÍTULO
     # ===================
     def mostrar_titulo(self):
+
         label_titulo = ttk.Label(
             self,
             text="Zona Fit (Clientes)",
@@ -222,6 +225,89 @@ class Interfaz(tk.Tk):
                     cliente.estado,
                 ),
             )
+
+    def mostrar_botones(self):
+        self.frame_botones = ttk.Frame()
+        # estilo a los botones
+        self.estilos.configure(
+            "Boton.TButton",
+            font=("Arial", 12, "bold"),
+            padding=(20, 15),
+            background="#005f73",
+        )
+
+        # BOTON GUARDAR
+        self.boton_guardar = ttk.Button(
+            self.frame_botones,
+            text="Guardar",
+            style="Boton.TButton",
+            width=15,
+            command=self.validar_cliente,
+        )
+        self.boton_guardar.grid(row=0, column=0, padx=10)
+
+        # BOTON ELIMINAR
+        self.boton_eliminar = ttk.Button(
+            self.frame_botones,
+            text="Eliminar",
+            style="Boton.TButton",
+            width=15,
+            command=self.eliminar_cliente,
+        )
+        self.boton_eliminar.grid(row=0, column=1, padx=10)
+
+        # BOTON ACTUALIZAR
+        self.boton_actualizar = ttk.Button(
+            self.frame_botones,
+            text="Actualizar",
+            style="Boton.TButton",
+            width=15,
+            command=self.actualizar_cliente,
+        )
+        self.boton_actualizar.grid(row=0, column=2, padx=10)
+
+        # BOTON LIMPIAR
+        self.boton_limpiar = ttk.Button(
+            self.frame_botones,
+            text="Limpiar",
+            style="Boton.TButton",
+            width=15,
+            command=self.limpiar_campos,
+        )
+        self.boton_limpiar.grid(row=0, column=3, padx=10)
+
+        # BOTON SALIR
+        self.boton_salir = ttk.Button(
+            self.frame_botones,
+            text="Salir",
+            style="Boton.TButton",
+            width=15,
+            command=self.salir,
+        )
+        self.boton_salir.grid(row=0, column=4, padx=5)
+
+        self.estilos.map("Boton.TButton", background=[("active", "#428475")])
+
+        # FRAME
+        self.frame_botones.grid(row=2, column=0, columnspan=2, pady=40)
+
+    def validar_cliente(self):
+        pass
+
+    def eliminar_cliente(self):
+        pass
+
+    def limpiar_campos(self):
+        pass
+
+    def actualizar_cliente(self):
+        pass
+
+    def salir(self):
+        respuesta = messagebox.askyesno("Salir", "¿Desea salir de la aplicación?")
+
+        if respuesta:
+            self.destroy()
 
 
 if __name__ == "__main__":
